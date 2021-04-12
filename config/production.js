a3f0ef593c1516c1d14a030459c7974a26278d63
rafabernardo <rafael.rbernardo@mercadolivre.com>
2021-04-12 02:29:54 -0300

    refactor: improve production config

--- config/production.js
+++ config/production.js
@@ -7,14 +7,6 @@ const subscriberPath = join(__dirname, '../dist/**/subscriber/*.js')
 module.exports = {
   dbConfig: {
     main: {
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'mysql',
-      password: 'supersecret',
-      database: 'ihurry',
-      synchronize: true,
-      logging: false,
       entities: [entityPath],
       subscribers: [subscriberPath],
       migrations: [migrationPath]

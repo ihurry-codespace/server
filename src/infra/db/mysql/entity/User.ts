b45f803870cbd87d89a93097caf68857cbe038e6
rafabernardo <rafael.rbernardo@mercadolivre.com>
2021-04-12 02:23:31 -0300

    refactor: add comments to user entity

--- src/infra/db/mysql/entity/User.ts
+++ src/infra/db/mysql/entity/User.ts
@@ -1,11 +1,25 @@
 import { Entity, Column, PrimaryColumn } from 'typeorm'
 import { EntityUpdateControl } from './EntityUpdateControl'
 
+/**
+ * Enum user statuses
+ * @readonly
+ * @enum {string}
+ */
 export enum UserStatus {
+  /** The default status for new records. To log in, a user record must be active. */
   ACTIVE = 'active',
+
+  /** Users cannot log in. Inactive user records do not appear in select value lists and cannot be associated with new records. */
   INACTIVE = 'inactive',
+
+  /** Users cannot log in. */
   BLOCKED = 'blocked',
+
+  /** The user was deleted but the user name has been retained. */
   DELETED = 'deleted',
+
+  /** The default status for user records that are created using self-registration. */
   NEWREG = 'newreg',
 }
 

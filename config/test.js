7d0fec32f20bd56fcacff694e3dfbb56ebb1dc3c
rafabernardo <rafael.rbernardo@mercadolivre.com>
2021-04-12 00:00:05 -0300

    fix: integration tests

---
+++ config/test.js
@@ -0,0 +1,11 @@
+module.exports = {
+  appConfig: {
+    PORT: 8080,
+    HOST: '127.0.0.1'
+  },
+  tokenConfig: {
+    privateKey: 'BO@&gZt*7)m|q-W?`U7q?6P+$Y$]f:"Qqs-jAW"xo,q0H>6&X@,aZdJK&Ku*M>j',
+    expiresIn: '24h',
+    algorithm: 'HS256'
+  }
+}
